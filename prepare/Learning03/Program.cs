2df9c2de6830c335bbb978d31843b510a871a84b
bmthornock <b.t.thornock@gmail.com>
2023-02-01 18:46:40 -0600

    Lesson3 Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        f1.GetFractionString();
+        Console.WriteLine(f1.GetDecimalValue());
+        f2.GetFractionString();
+        Console.WriteLine(f2.GetDecimalValue());
+        f3.GetFractionString();
+        Console.WriteLine(f3.GetDecimalValue());
+        f4.GetFractionString();
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
