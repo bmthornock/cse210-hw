ae71d50b984cf1e96923c772654790201daddd2b
bmthornock <b.t.thornock@gmail.com>
2023-02-06 19:07:35 -0600

    Lesson 6 prove skeleton

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+        bool program = true;
+
+        while program == true {
+
+            Scripture myScripture = new Scripture();
+
+            
+        }
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+public class Scripture{
+
+    private string [,] _scriptures = new string [9,5] {
+    {"Moses", "1", "39", "", "For behold, this is my work and my glory—to bring to pass the immortality and eternal life of man.",},
+    {"Genesis", "39", "9", "", "There is none greater in this house than I; neither hath he kept back any thing from be but thee, becaus though art his wife: how then can I do this great wickedness, and sin against God?",},
+    {"Joshua", "1", "8", "", "This book of the law shall not depart out of thy mouth; but though shalt medicate therin day and night, that thou mayest observe to do according to all that is written therein: for then thou shalt make thy way prosperous, and then thou shalt have good success."},
+    {"Daniel", "2", "44", "45", "And in the days of these kings shall the God of heaven set up a kindgom which shall never be destroyed: and the kingdom shall not be left to other people, but it shall break in pieces and consume all these kingdoms, and it shall stand forever. Forasmuch as thou sawest that the stone was cut out of the mountain without hands, and that it brake in pieces the iron, the brass, the clay, the silver, and the gold; the great God hath made known to the king what shall come to pass hereafter: and the dream is certain, and the interpretation thereof sure."},
+    {"Mathew", "5", "14", "16", "Ye are the light of the world. A city that is set on an hill and cannot be hid. Niether do men light a candle, and put it under a bushel, but on a candlestick; and it giveth light unto all that are in the house. Let your light so shine before men, that they may see your good works, and glorify your Father which is in heaven.",},
+    {"Matthew", "25", "40", "", "And the King shall answer and say unto them, Verily I say unto you, Inasmuch as ye have done it unto it unto one of the least of these my bretheren, ye have done it unto me."},
+    {"Acts", "7", "55", "56", "But he, being full of the Holy Ghost, looked up steadfastly into heaven, and saw the glory of God, and Jesus standing on the right hand of God, And said, Behold, I see the heavens opened, and the Son of man standing on the right hand of God."},
+    {"2 Timothy", "3", "1", "5", "This know also, that in the last days perilous times shall come. For men shall be lovers of their own selves, covetous, boasters, proud, blasphemers, disobedient to parents, unthankful, unholy, Without natural affection, trucebreakers, false accusers, incontinent, fierce, dispisers of those that are good, Traitors, heady, highminded, lovers of pleasures more than lovers of God; Having a form of Godliness, but denying the power thereof: from such turn away."},
+    {"Hebrew", "5", "4", "", "And no man taketh this honour unto himself, but he that is called of God, as was Aaron",},
+};
+
+    
+
+}
